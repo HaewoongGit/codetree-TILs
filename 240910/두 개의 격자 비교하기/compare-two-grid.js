b6901d9c25bd3fd316d34998e505925a0bf67d98
HaewoongGit <107612118+HaewoongGit@users.noreply.github.com>
2024-09-10 18:45:33 +0900

    Title: 두 개의 격자 비교하기, Time: 299ms, Memory: 14MB, Status: Passed - CodeTree

--- 240910/두 개의 격자 비교하기/compare-two-grid.js
+++ 240910/두 개의 격자 비교하기/compare-two-grid.js
@@ -1,14 +1,33 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(row => row.split(' ').map(Number));
 
-const answerArr = [];
+// const answerArr = [];
 
-for(let i = 1; i < input[0][1] + 1; i++) {
-    const newArr = [];
-    for(let j = 0; j < input[0][0]; j++) {
-        input[i][j] === input[input[0][0]+i][j] ? newArr.push(0) : newArr.push(1);
+// for(let i = 1; i < input[0][1] + 1; i++) {
+//     const newArr = [];
+//     for(let j = 0; j < input[0][0]; j++) {
+//         input[i][j] === input[input[0][0]+i][j] ? newArr.push(0) : newArr.push(1);
+//     }
+//     answerArr.push(newArr);
+// }
+
+// console.log(answerArr.map(row => row.join(' ')).join('\n'));
+
+
+
+
+const [n, m] = input[0];
+const grid1 = input.slice(1, n + 1);
+const grid2 = input.slice(n + 1);
+
+const result = [];
+
+for(let i = 0; i < n; i++) {
+    const row = [];
+    for(let j = 0; j < m; j++) {
+        row.push(grid1[i][j] === grid2[i][j] ? 0 : 1);
     }
-    answerArr.push(newArr);
+    result.push(row);
 }
 
-console.log(answerArr.map(row => row.join(' ')).join('\n'));
+console.log(result.map(row => row.join(' ')).join('\n'));
