1484dd00c9838b459ad575aae431e27cfc0d731b
HaewoongGit <107612118+HaewoongGit@users.noreply.github.com>
2024-09-18 21:39:07 +0900

    Title: 아래로 사각형 채우기, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

--- 240918/아래로 사각형 채우기/filling-rectangle-with-downflow.js
+++ 240918/아래로 사각형 채우기/filling-rectangle-with-downflow.js
@@ -5,7 +5,7 @@ const n = Number(fs.readFileSync("/dev/stdin").toString());
 for(let i = 1; i <= n; i++) {
     let str = '';
     for(let j = 0; j < n; j++) {
-        str += ((i + 5 * j) + ' ');
+        str += ((i + n * j) + ' ');
     }
     console.log(str);
 }
